29cf09fd20aecd0113e3c801b51d073092fa1e3a
Kuldeep-sahu6260 <79197859+Kuldeep-sahu6260@users.noreply.github.com>
2022-06-28 14:58:59 +0530

    demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-// import Email from './Email';
-import "./index.css";
 
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
